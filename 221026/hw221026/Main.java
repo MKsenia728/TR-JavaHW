89688fe099c719e361e67aa2a6062fbd24151b52
MKsenia728 <mksenia728@gmail.com>
2022-10-28 20:21:35 +0200

    Усовершенствовано ДЗ от 26.10.2022

--- 221026/hw221026/Main.java
+++ 221026/hw221026/Main.java
@@ -2,55 +2,45 @@ package hw221026;
 
 public class Main {
 
-    public static int counterForSimple(int numberOfFloors) {
+    public static int counterFor(int numberOfFloors, int floorUp, int floorDown) {
         int counter = 0;
-        for (int i = 0; i < numberOfFloors; i = i + 5 - 1) {
+        for (int i = 0; i < numberOfFloors; i = i + floorUp) {
             counter++;
+            i = i - floorDown;
         }
         return counter;
     }
 
-    public static int counterForHeavy(int numberOfFloors) {
-        int counter = 1;
-        int j = 0;
-        for (int i = 0; i <= numberOfFloors; i++, j++) {
-            if (j > 0 && j % 5 == 0) {
-                counter++;
-                i--;
-            }
-        }
-        return counter;
-    }
-
-    public static int counterWhile(int numberOfFloors) {
+    public static int counterWhile(int numberOfFloors, int floorUp, int floorDown) {
         int counter = 0;
         int i = 0;
         while (i < numberOfFloors) {
-            i = i + 5 - 1;
+            i = i + floorUp - floorDown;
             counter++;
         }
         return counter;
     }
 
-    public static int counterDoWhile(int numberOfFloors) {
+    public static int counterDoWhile(int numberOfFloors, int floorUp, int floorDown) {
         int counter = 0;
-        int i = 0;
-        do {
-            i = i + 5 - 1;
-            counter++;
-        } while (i < numberOfFloors);
+        if (numberOfFloors > 0) {
+            int i = 0;
+            do {
+                i = i + floorUp - floorDown;
+                counter++;
+            } while (i < numberOfFloors);
+        }
         return counter;
     }
 
     public static void main(String[] args) {
 
         int numberOfFloors = 163;
+        int floorUp = 5;
+        int floorDown = 1;
 
-        if (numberOfFloors > 0) {
-            System.out.println("Количество подъемов по FOR : " + counterForSimple(numberOfFloors));
-            System.out.println("Количество подъемов по FOR (heavy): " + counterForHeavy(numberOfFloors));
-            System.out.println("Количество подъемов по WHILE : " + counterWhile(numberOfFloors));
-            System.out.println("Количество подъемов по DO-WHILE : " + counterDoWhile(numberOfFloors));
-        }
+        System.out.println("Количество подъемов по FOR : " + counterFor(numberOfFloors,floorUp,floorDown));
+        System.out.println("Количество подъемов по WHILE : " + counterWhile(numberOfFloors,floorUp,floorDown));
+        System.out.println("Количество подъемов по DO-WHILE : " + counterDoWhile(numberOfFloors,floorUp,floorDown));
     }
 }
