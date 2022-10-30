34760b365defaf779008e55a81fb21fbed0a3c1a
MKsenia728 <mksenia728@gmail.com>
2022-10-30 08:28:04 +0100

    Correct conditions

--- 221026/hw221026/Main.java
+++ 221026/hw221026/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static int counterFor(int numberOfFloors, int floorUp, int floorDown) {
         int counter = 0;
-        for (int i = 0; i < numberOfFloors; i = i - floorDown) {
+        for (int i = 0; i <= numberOfFloors - floorDown; i = i - floorDown) {
             counter++;
             i = i + floorUp;
         }
@@ -14,7 +14,7 @@ public class Main {
     public static int counterWhile(int numberOfFloors, int floorUp, int floorDown) {
         int counter = 0;
         int i = 0;
-        while (i < numberOfFloors) {
+        while (i <= numberOfFloors - floorDown) {
             i = i + floorUp - floorDown;
             counter++;
         }
@@ -28,16 +28,16 @@ public class Main {
             do {
                 i = i + floorUp - floorDown;
                 counter++;
-            } while (i < numberOfFloors);
+            } while (i <= numberOfFloors - floorDown);
         }
         return counter;
     }
 
     public static void main(String[] args) {
 
-        int numberOfFloors = 163;
-        int floorUp = 5;
-        int floorDown = 1;
+        int numberOfFloors = 164;
+        int floorUp = 7;
+        int floorDown = 2;
 
         System.out.println("Количество подъемов по FOR : " + counterFor(numberOfFloors,floorUp,floorDown));
         System.out.println("Количество подъемов по WHILE : " + counterWhile(numberOfFloors,floorUp,floorDown));
